aa69ca51ebc3a62ccf07f8e38526e221597cd0a4
timbo-dev <dev@nathanrocha.com.br>
2023-07-08 13:21:37 -0300

    ✨ feat: add download package and export path

--- install.sh
+++ install.sh
@@ -9,14 +9,59 @@ OWNER_USER="timbo-dev"
 REPOSITORY_NAME="binara"
 BRANCH="main"
 
+ROOT_PATH=$PWD
+
+home() {
+    local path=${1}
+    echo $BINARA_HOME/$path
+}
+
+bin() {
+    local path=${1}
+    echo $(home $BIN_DIR/$path)
+}
+
+package() {
+    local path=${1}
+    echo $(home $PACKAGES_DIR/$path)
+}
+
 downloadFile() {
     local filePath=${1}
-    
+
     if ! [ -z $2 ]; then
         local output="--output-document ${2}"
     fi
 
-    wget --quiet --show-progress "$RAW_URL/$OWNER_USER/$REPOSITORY_NAME/$BRANCH/$filePath" $output
+    wget --quiet                             \
+         --show-progress                     \
+         "$RAW_URL/$OWNER_USER/$REPOSITORY_NAME/$BRANCH/$filePath" $output
+}
+
+downloadPackage() {
+    local packages=$(getSource "${1}")
+    local lastPath=$PWD
+    
+    cd $ROOT_PATH && cd $(package)
+
+    for package in $packages; do
+        folderPackage=$(dirname $package)
+        packagePath=$(echo $package | sed -E "s/$PACKAGES_DIR\///g")
+
+        mkdir -p $BINARA_HOME/$folderPackage
+        downloadFile $package $packagePath
+    done
+
+    cd $lastPath
+    echo
+}
+
+makeLinkBin() {
+    local bin=${1}
+    local linkTarget="$(bin $(basename $bin))"
+
+    echo "Linking: $1 to $linkTarget"
+    ln -sf "$bin" "$linkTarget"
 }
 
 getSource() {
@@ -25,9 +70,8 @@ getSource() {
     if ! [ -z ${2} ]; then
         local fileTarget="/${2}"
     fi
-
-    local contentFile=$(cat $BINARA_HOME/$SOURCE_MAP_FILE) | grep "$PACKAGES_DIR/$packageTarget$fileTarget"
-
+    
+    local contentFile=$(cat $(home $SOURCE_MAP_FILE) | grep "$PACKAGES_DIR/$packageTarget$fileTarget")
     echo "$contentFile"
 }
 
@@ -42,10 +86,6 @@ if [ -d $BINARA_HOME ]; then
     exit 1
 fi
 
-createDir $BINARA_HOME
-createDir $BINARA_HOME/$BIN_DIR
-createDir $BINARA_HOME/$PACKAGES_DIR
-
 WGET_COMMAND=$(whereis wget)
 
 if [ "$WGET_COMMAND" == "wget:" ]; then
@@ -54,8 +94,23 @@ if [ "$WGET_COMMAND" == "wget:" ]; then
     exit 1
 fi
 
-downloadFile $SOURCE_MAP_FILE $BINARA_HOME/$SOURCE_MAP_FILE
-getSource core
+createDir $(home)
+createDir $(bin)
+createDir $(package)
+
+echo
+
+cd $BINARA_HOME
+
+downloadFile $SOURCE_MAP_FILE $SOURCE_MAP_FILE
+
+echo
+
+downloadPackage core
+downloadPackage binara
+
+makeLinkBin $(package binara/binara)
+makeLinkBin $(package core/core)
 
 SHELL_NAME=$(basename $SHELL)
 
@@ -63,20 +118,31 @@ showExportMessage() {
     echo "manually paste the $BINARA_HOME variable into your shell configuration file:"
     echo
     echo 'export $BINARA_HOME=$HOME/.binara'
+    exit 1
 }
 
 if [ ! -z $SHELL_NAME ]; then
     SHELL_RC=".$SHELL_NAME""rc"
     RC_PATH="$HOME/$SHELL_RC"
 
-    echo
     echo "Shell '$SHELL_NAME' detected, trying to insert \$BINARA_HOME environment on '$HOME/$SHELL_RC'"
 
     if [ ! -f "$RC_PATH" ]; then
         echo "error: cannot find the file '$RC_PATH'"
         showExportMessage
     fi
+    
+    RC_CONTENT=$(cat "$RC_PATH")
+    EXPORT_BINARA_HOME='export BINARA_HOME=$HOME/.binara'
+    EXPORT_BINARA_BIN_PATH='export PATH="$BINARA_HOME/bin:$PATH"'
 
+    if ! [[ "$RC_CONTENT" =~ "$EXPORT_BINARA_HOME" ]]; then
+        echo $EXPORT_BINARA_HOME >> $RC_PATH
+    fi
+
+    if ! [[ "$RC_CONTENT" =~ "$EXPORT_BINARA_BIN_PATH" ]]; then
+        echo $EXPORT_BINARA_BIN_PATH >> $RC_PATH
+    fi
 else
     echo "error: shell name not found."
     showExportMessage
